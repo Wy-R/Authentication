bd7d2c925a8bc9842b9b21e60ce486a147bc0949
wuyangcherish <wuyang_cherish@yahoo.com>
2017-04-10 10:54:58 +0800

    delete a.js && add another way use axios

--- src/api/api.js
+++ src/api/api.js
@@ -1,16 +1,36 @@
 import Vue from 'vue'
 import axios from 'axios'
 
+var getAxios = function(){
+	return axios;
+}
+
 function installAxios(Vue, options) {
 	if (installAxios.installed) return
 
 	installAxios.installed = true
 
+	if(!axios){
+		console.error("Please install axios")
+	}
+
+	/** 组件注入的方式 **/
 	Vue.mixin({
 		created() {
 			return this.$axios = axios;
 		}
 	})
+	// 调用方式直接是  this.$axios -->  返回 axios 插件
+
+	/** or  添加实例的方法**/
+	Object.defineProperties(Vue.prototype,{
+		$axios:{
+			get(){
+				return getAxios
+			}
+		}
+	})
+	
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
